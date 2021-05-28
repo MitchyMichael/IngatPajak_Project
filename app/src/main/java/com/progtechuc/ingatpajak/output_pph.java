8709dec8222a99f3d2884fe70add16a4fedd07ab
MitchyMichael <84000679+MitchyMichael@users.noreply.github.com>
2021-05-28 22:07:20 +0700

    output pph done

--- app/src/main/java/com/progtechuc/ingatpajak/output_pph.java
+++ app/src/main/java/com/progtechuc/ingatpajak/output_pph.java
@@ -2,13 +2,32 @@ package com.progtechuc.ingatpajak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class output_pph extends AppCompatActivity {
+
+    private Button button_menu_outputPPh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_output_pph);
+        initView();
+
+        button_menu_outputPPh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), HomeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initView() {
+        button_menu_outputPPh = findViewById(R.id.button_menu_outputPPh);
     }
 }
